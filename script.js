17e69aca3ec5b78d12ca16d04c9b9fd41eae6191
Nirbhay <l.nirbhay@gmail.com>
2022-11-03 13:04:51 +0530

    add intial load song functionality

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ const musicContainer = document.getElementById('music-container');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+const title = document.getElementById('title');
+const audio = document.getElementById('audio');
+const cover = document.getElementById('cover');
 
+// Song titles
 const songs = ['hey', 'summer', 'ukulele'];
+
+// Keep track of song
+let songIndex = 0;
+
+// Initally load song details into DOM
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
 
 function playSong() {
   musicContainer.classList.add('play');
