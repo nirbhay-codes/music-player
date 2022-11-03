d347c78a3a783af0e9e43226ba2dce77bb2fe7fa
Nirbhay <l.nirbhay@gmail.com>
2022-11-03 12:54:05 +0530

    add play pause ui change

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const nextBtn = document.getElementById('next');
 
 const songs = ['hey', 'summer', 'ukulele'];
 
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+}
+
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+}
+
 playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play');
 
